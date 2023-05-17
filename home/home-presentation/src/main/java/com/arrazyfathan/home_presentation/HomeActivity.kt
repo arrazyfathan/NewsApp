50989e06f7ab01ffc9cf3a398d09c7307ddb9dad
arrazyfathan <arrazy.rabbani266@gmail.com>
2023-05-17 13:55:33 +0700

    Add badge count bookmark item bottom navigation bar

--- home/home-presentation/src/main/java/com/arrazyfathan/home_presentation/HomeActivity.kt
+++ home/home-presentation/src/main/java/com/arrazyfathan/home_presentation/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.arrazyfathan.home_presentation
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -12,14 +13,9 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
 
-    companion object {
-        fun launchActivity(activity: Activity) {
-            val intent = Intent(activity, HomeActivity::class.java)
-            activity.startActivity(intent)
-        }
-    }
-
     private lateinit var binding: ActivityHomeBinding
+
+    private val viewModel: TopHeadlinesViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +37,25 @@ class HomeActivity : AppCompatActivity() {
             if (item.itemId == currentDestination?.id) return@setOnItemSelectedListener false
             navController.navigate(item.itemId)
             return@setOnItemSelectedListener true
+        }
+
+        viewModel.bookmarkedArticles.observe(this) { articles ->
+            binding.bottomNavigationBar.getOrCreateBadge(R.id.bookmarkFragment).apply {
+                if (articles.isNotEmpty()) {
+                    backgroundColor = getColor(R.color.black)
+                    isVisible = true
+                    number = articles.size
+                } else {
+                    isVisible = false
+                }
+            }
+        }
+    }
+
+    companion object {
+        fun launchActivity(activity: Activity) {
+            val intent = Intent(activity, HomeActivity::class.java)
+            activity.startActivity(intent)
         }
     }
 }
