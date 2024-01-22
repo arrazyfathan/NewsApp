0ff70010b710b7590458428f1cbee6a501e62555
arrazyfathan <arrazy.rabbani266@gmail.com>
2024-01-22 14:55:22 +0700

    Upgrade AGP to version 8.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,15 +30,16 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
     buildFeatures {
         viewBinding = true
     }
+    namespace = "com.example.newsapp"
 }
 
 dependencies {
--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
     dependencies {
         classpath(Deps.navigationSafeArgs)
         classpath(Deps.hiltAgp)
-        classpath("com.android.tools.build:gradle:7.2.2")
+        classpath("com.android.tools.build:gradle:8.1.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
     }
 }
